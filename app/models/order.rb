99c3ccf7537b8e4cd8a72e0cbd965414eb4c4e01
usho1024 <usho1024@gmail.com>
2021-10-20 10:08:53 +0000

    モデルとコントローラーとルーティング作成完了！

--- app/models/order.rb
+++ app/models/order.rb
@@ -1,4 +1,9 @@
 class Order < ApplicationRecord
+  
+  has_many :order_details
+  belongs_to :customer
+  
   enum payment_method: { credit_card: 0, transfer: 1 }
   enum status: { wait: 0, confirm: 1, making: 2, packing: 3, done: 4 }
+  
 end
--- config/routes.rb
+++ config/routes.rb
@@ -1,12 +1,42 @@
 Rails.application.routes.draw do
 
-devise_for :customers,skip: [:passwords,], controllers: {
-  registrations: "customer/registrations",
-  sessions: 'customer/sessions'
-}
-
-devise_for :admin, skip: [:registrations, :passwords] ,controllers: {
-  sessions: "admin/sessions"
-}
+  devise_for :customers,skip: [:passwords,], controllers: {
+    registrations: "customer/registrations",
+    sessions: 'customer/sessions'
+  }
   
+  devise_for :admin, skip: [:registrations, :passwords] ,controllers: {
+    sessions: "admin/sessions"
+  }
+  
+  root :to => "customer/homes#top"
+  get "/about" => "customer/homes#about"
+
+  get "/customers/my_page" => "customer/customers#show"
+  get "/customers/edit" => "customer/customers#edit"
+  patch "/customers" => "customer/customers#update"
+  get "/customers/unsubscribe" => "customer/customers#unsubscribe"
+  patch "/customers/withdraw" => "customer/customers#withdraw"
+  
+  delete "/cart_items/destroy_all" => "customer/cart_items#destroy_all"
+  post "/orders/confirm" => "customer/orders#comfirm"
+  get "/orders/complete" => "customer/orders#complete"
+
+  scope module: :customer do
+    resources :items, only: [:index, :show]
+    resources :cart_items, only: [:index, :update, :destroy, :create]
+    resources :orders, only: [:index, :show, :new, :create]
+    resources :addresses, only: [:index, :edit, :create, :update, :destroy]
+  end
+
+  get "/admin" => "admin/homes#top"
+
+  namespace :admin do
+    resources :customers, only: [:index, :show, :edit, :update]
+    resources :items, except: [:destroy]
+    resources :genres, only: [:index, :create, :edit, :update]
+    resources :orders, only: [:show, :update]
+    resources :order_details, only: [:update]
+  end
+
 end

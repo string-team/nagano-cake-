571de5a6dff98078eb6410e6d22ccad3f661e6bc
EC2 Default User <ec2-user@ip-172-31-44-111.ec2.internal>
2021-10-23 11:22:08 +0000

    confirm途中経過

--- app/controllers/customer/orders_controller.rb
+++ app/controllers/customer/orders_controller.rb
@@ -1,2 +1,21 @@
 class Customer::OrdersController < ApplicationController
+  def new
+    # @customer = current_customer
+    @order = Order.new
+    # @address = DeliveryAddress.new
+  end
+  
+  def confirm
+    @order = Order.new(order_params)
+    @cart_items = CartItem.where(customer_id: current_customer.id)
+    
+  end
+  
+  
+  private
+  
+  
+  def order_params
+    params.require(:order).permit(:payment_method, :address, :shipping_cost, :postal_code, :name, :total_payment)
+  end
 end
--- app/models/order.rb
+++ app/models/order.rb
@@ -3,6 +3,8 @@ class Order < ApplicationRecord
   has_many :order_details
   belongs_to :customer
   
+  attachment :image
+  
   enum payment_method: { credit_card: 0, transfer: 1 }
   enum status: { wait: 0, confirm: 1, making: 2, packing: 3, done: 4 }
   

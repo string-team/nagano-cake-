3abb3e147440b789b52d37dc0ca54b507770385f
usho1024 <usho1024@gmail.com>
2021-10-27 12:23:49 +0000

    [Update] 会員側と管理者側の注文一覧画面を作成

--- app/controllers/customer/orders_controller.rb
+++ app/controllers/customer/orders_controller.rb
@@ -1,27 +1,27 @@
 class Customer::OrdersController < ApplicationController
   # before_action :authenticate_customer!
   # before_action :order_new?, only: [:new]
-  
+
   def new
     @order = Order.new
     @addresses = Address.where(customer_id: current_customer.id)
   end
-  
+
   def confirm
-    
+
     @order = Order.new(order_params)
     @cart_items = CartItem.where(customer_id: current_customer.id)
     @total = 0
     if params[:address_number] == "1"
       @order.postal_code = current_customer.postal_code
       @order.address = current_customer.address
-      @order.name = current_customer.first_name+current_customer.last_name
+      @order.name = current_customer.last_name + current_customer.first_name
     elsif params[:address_number] == "2"
       @address = Address.find(params[:order][:id])
       @order.postal_code = @address.postal_code
       @order.address = @address.address
       @order.name = @address.name
-    elsif params[:address_number] ==  "3" 
+    elsif params[:address_number] ==  "3"
       @order.address = params[:order][:address]
       @order.name = params[:order][:name]
       @order.postal_code = params[:order][:postal_code]
@@ -29,7 +29,7 @@ class Customer::OrdersController < ApplicationController
       render 'new'
     end
   end
-  
+
   def create
     @order = Order.new(order_params)
     @order.customer_id = current_customer.id
@@ -45,27 +45,27 @@ class Customer::OrdersController < ApplicationController
     @cart_items.destroy_all
     redirect_to orders_complete_path
   end
-  
-  
+
+
   def complete
   end
-  
+
   def show
     @order = Order.find(params[:id])
     @order_details = @order.order_details
     @total = 0
   end
-  
+
   def index
-    @order = Order.all
+    @orders = current_customer.orders
   end
-  
+
   private
-  
+
   def order_new?
     redirect_to cart_items_path, notice: "カートに商品を入れてください。" if current_customer.cart_items.blank?
   end
-  
+
   def order_params
     params.require(:order).permit(:payment_method, :address, :shipping_cost, :postal_code, :name, :total_payment)
   end
